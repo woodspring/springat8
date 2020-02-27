4b1997a57e3933e745ebeb2462b95852658338ae
Woodspringson <woodspring.toronto@gmail.com>
2020-02-27 15:51:45 -0500

    GNode

---
+++ src/main/java/woodspring/springat8/model/GraphNode.java
@@ -0,0 +1,11 @@
+package woodspring.springloop.model;
+
+public class GraphNode<T> { //extends Comparable<T> {
+
+//	@Override
+//	public int compareTo(T o) {
+//		// TODO Auto-generated method stub
+//		return 0;
+//	}
+
+}
